b511460479d8dbcc365f8f558bf274348b23eaa7
Yuji Kiriki <yujikiriki@gmail.com>
2013-01-22 17:33:48 -0500

    If changed for Option/SOme/None

--- src/main/scala/co/s4n/poller/infrastructure/persistence/PollerCollectionDataServices.scala
+++ src/main/scala/co/s4n/poller/infrastructure/persistence/PollerCollectionDataServices.scala
@@ -11,28 +11,25 @@ import com.mongodb.DBObject
 object PollerCollectionDataServices extends MongoDBLoan with Logging {
   
   def isReportDone( aCollName: String ): Boolean = {
-    /* The MongoDB operations */
     def f( conn: MongoConnection ): Boolean = {
       val coll: MongoCollection = conn( databaseName )( aCollName )
       val doc: DBObject = MongoDBObject( "tipoDocumento" -> "done" )
-      if( 1 == coll.findOne( doc ).size ) {
-        coll.remove( doc )
-        log.debug( "Documento con estado 'done' eliminado" )
-        true
+      coll.findOne( doc ) match {
+        case Some( foundDoc ) => {
+       	  coll.remove( foundDoc )
+          true  
+        }
+        case None => false
       }
-      false
     }
-    /* Loan call */
     loan( f )
   }
   
   def removeReportCollection( aCollName: String ): Unit = {
-    /* The MongoDB operations */
     def f( conn: MongoConnection ): Unit = {
       val coll: MongoCollection = conn( databaseName )( aCollName )
       coll.drop
     }
-    /* Loan call */
     loan( f )
   }
 }
