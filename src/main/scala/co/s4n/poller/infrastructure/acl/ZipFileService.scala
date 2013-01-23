46b3eaa602c7185037339450daa484c8e9b1fd52
Yuji Kiriki <yujikiriki@gmail.com>
2013-01-23 17:31:48 -0500

    * After the zip creation, the files used are deleted. * The zip file does not include the folders.

--- src/main/scala/co/s4n/poller/infrastructure/acl/ZipFileService.scala
+++ src/main/scala/co/s4n/poller/infrastructure/acl/ZipFileService.scala
@@ -1,22 +1,48 @@
 package co.s4n.poller.infrastructure.acl
 import java.io.{ BufferedInputStream, FileInputStream, FileOutputStream }
 import java.util.zip.{ ZipEntry, ZipOutputStream }
+import java.io.File
+import scala.util.{ Try, Success, Failure }
+import co.s4n.poller.infrastructure.Logging
 
-object ZipFileService {
+object ZipFileService extends Logging {
 
-  def zipFile( out: String, files: Iterable[String] ) = {
-    val zip = new ZipOutputStream( new FileOutputStream( out ) )
-    files.foreach { name =>
-      zip.putNextEntry( new ZipEntry( name ) )
-      val in = new BufferedInputStream( new FileInputStream( name ) )
-      var b = in.read()
-      while ( b > -1 ) {
-        zip.write( b )
-        b = in.read()
+  def zipFile( out: String, files: List[String] ) = {
+    try {
+      val zip = new ZipOutputStream( new FileOutputStream( out ) )
+      files.foreach { name =>
+        val file: File = new File( name )
+        zip.putNextEntry( new ZipEntry( file.getName ) )
+        val in = new BufferedInputStream( new FileInputStream( name ) )
+        var b = in.read
+        while ( b > -1 ) {
+          zip.write( b )
+          b = in.read()
+        }
+        in.close
+        zip.closeEntry
       }
-      in.close()
-      zip.closeEntry()
-    }
-    zip.close()
+      zip.close
+      /* If the zip file is created, delete the source */
+      deleteFiles( files )
+    } catch {
+      case e: Exception => { 
+        println( e )
+        log.error( e.getMessage )
+      }
+    }    
+  }
+  
+  private def deleteFiles( paths: List[String] ): Unit = {
+    paths.foreach( path => {
+      val file = Try( new File( path ) )
+      file match {
+        case Success( f ) => f.delete
+        case Failure( ex ) => { 
+          println( ex.getMessage )
+          log.error( ex.getMessage )
+        }
+      }
+    })
   }
 }
--- src/test/scala/co/s4n/poller/ZipTest.scala
+++ src/test/scala/co/s4n/poller/ZipTest.scala
@@ -9,9 +9,8 @@ import co.s4n.poller.infrastructure.acl.ZipFileService
 @RunWith( classOf[JUnitRunner] )
 class ZipTest extends FunSuite {
 
-  ignore( "Zip multiple files" ) {
-    val fileList: List[String] = List( "/home/yuji/Desktop/5.todo", "/home/yuji/Desktop/4.todo", "/home/yuji/Desktop/3.todo", "/home/yuji/Desktop/2.todo", "/home/yuji/Desktop/Poller.todo" )
+  test( "Zip multiple files" ) {
+    val fileList: List[String] = List( "/home/yuji/Desktop/1.txt", "/home/yuji/Desktop/2.txt", "/home/yuji/Desktop/3.txt", "/home/yuji/Desktop/4.txt", "/home/yuji/Desktop/5.txt" )
     ZipFileService.zipFile( "/home/yuji/Desktop/test.zip" , fileList )
   }
-  
 }
